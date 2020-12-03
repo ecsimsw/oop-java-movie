ae1053a57af4679672b2af3006dc5e77fd72f0fb
ecsimsw <ecsimsw@gmail.com>
2020-12-04 04:40:36 +0900

    docs : 기능 목록 README 작성

---
+++ src/main/java/MovieApplication.java
@@ -0,0 +1,17 @@
+import sample.domain.Movie;
+import sample.domain.MovieRepository;
+import sample.view.InputView;
+import sample.view.OutputView;
+
+import java.util.List;
+
+public class MovieApplication {
+    public static void main(String[] args) {
+        List<Movie> movies = MovieRepository.getMovies();
+        OutputView.printMovies(movies);
+
+        int movieId = InputView.inputMovieId();
+
+        // TODO 구현 진행
+    }
+}
