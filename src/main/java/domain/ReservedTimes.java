6b164ca65b604c392465eabd111725086bea3b88
ecsimsw <ecsimsw@gmail.com>
2020-12-08 05:27:33 +0900

    feat : 사용자 예약 시간을 담을 ReservedTimes 구현 / 예약 시간 유효 확인

---
+++ src/main/java/domain/ReservedTimes.java
@@ -0,0 +1,45 @@
+package domain;
+
+import utils.DateTimeUtils;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReservedTimes {
+    private LocalDateTime firstTime;
+    private LocalDateTime lastTime;
+
+    public ReservedTimes(){
+
+    }
+
+    public void checkValidTime(LocalDateTime newTime){
+        if(firstTime == null){
+            return;
+        }
+
+        if(DateTimeUtils.isOneHourWithinRange(firstTime, newTime)
+                || DateTimeUtils.isOneHourWithinRange(lastTime, newTime)){
+            throw new IllegalArgumentException("1시간 이상 차이나는 시간 예약 불가");
+        }
+    }
+
+    public void addNew(LocalDateTime newTime){
+        if(firstTime == null){
+            firstTime = newTime;
+            lastTime = newTime;
+            return;
+        }
+
+        if(newTime.isBefore(firstTime)){
+            firstTime = newTime;
+            return;
+        }
+
+        if(newTime.isAfter(lastTime)){
+            lastTime = newTime;
+            return;
+        }
+    }
+}
--- src/main/java/domain/Ticketing.java
+++ src/main/java/domain/Ticketing.java
@@ -3,21 +3,17 @@ package domain;
 import view.InputView;
 import view.OutputView;
 
-import java.time.LocalDateTime;
-import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class Ticketing {
     private int totalPrice = 0;
-    private final List<LocalDateTime> reservedTimes = new ArrayList<>();
+    private final ReservedTimes reservedTimes = new ReservedTimes();
 
     public Ticketing(){}
 
     public void run(){
         List<Movie> movies = MovieRepository.getMovies();
         OutputView.printMovies(movies);
-
         do{
             makeReservation();
         }while(InputView.askTicketingMore());
@@ -25,15 +21,10 @@ public class Ticketing {
 
     public void makeReservation(){
         Movie movie = InputView.getMovie();
-        PlaySchedule playSchedule = getPlaySchedule(movie);
+        PlaySchedule playSchedule = InputView.getSchedule(movie, reservedTimes);
+        reservedTimes.addNew(playSchedule.getStartDateTime());
+
         int numberOfPeople = InputView.inputNumberOfPeople(playSchedule);
         totalPrice += movie.calculatePrice(numberOfPeople);
-    }
-
-    private PlaySchedule getPlaySchedule(Movie movie){
-        PlaySchedule playSchedule = InputView.getSchedule(movie, reservedTimes);
-        reservedTimes.add(playSchedule.getStartDateTime());
-        Collections.sort(reservedTimes);
-        return playSchedule;
     }
 }
