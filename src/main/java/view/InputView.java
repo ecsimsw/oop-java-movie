51fdb80be4d1e81dc27f66630093164aee52872f
ecsimsw <ecsimsw@gmail.com>
2020-12-08 03:43:40 +0900

    feat : 예약 영화, 시간, 인원 입력 받기 구현

--- src/main/java/view/InputView.java
+++ src/main/java/view/InputView.java
@@ -3,10 +3,47 @@ package view;
 import java.util.Scanner;
 
 public class InputView {
+    private InputView(){}
+
     private static final Scanner scanner = new Scanner(System.in);
 
     public static int inputMovieId() {
-        System.out.println("## 예약할 영화를 선택하세요.");
-        return scanner.nextInt();
+        try{
+            OutputView.printMsg("## 예약할 영화를 선택하세요.\n");
+            return getInteger();
+        }catch (Exception e){
+            return inputMovieId();
+        }
+    }
+
+    public static int inputTimeId() {
+        try{
+            OutputView.printMsg("## 예약할 시간를 선택하세요.\n");
+            return getInteger();
+        }catch (Exception e){
+            return inputMovieId();
+        }
+    }
+
+    public static int inputNumberOfPeople() {
+        try{
+            OutputView.printMsg("## 예약할 인원을 입력하세요.\n");
+            return getInteger();
+        }catch (Exception e){
+            return inputMovieId();
+        }
+    }
+
+    private static int getInteger(){
+        try{
+            return Integer.parseInt(getInput());
+        }catch (Exception e){
+            OutputView.printMsg("숫자를 입력하세요.");
+            return getInteger();
+        }
+    }
+
+    private static String getInput(){
+        return scanner.nextLine();
     }
 }
--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -5,9 +5,15 @@ import domain.Movie;
 import java.util.List;
 
 public class OutputView {
+    private OutputView(){}
+
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
             System.out.println(movie);
         }
+    }
+
+    public static void printMsg(Object msg){
+        System.out.print(msg);
     }
 }
