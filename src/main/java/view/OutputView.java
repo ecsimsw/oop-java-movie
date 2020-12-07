c5f49ce0a95234f0a7bb45e2a080460dd4fa3cc9
ecsimsw <ecsimsw@gmail.com>
2020-12-08 06:07:24 +0900

    feat : 결제 진행 메시지 출력 구현

--- src/main/java/view/OutputView.java
+++ src/main/java/view/OutputView.java
@@ -1,27 +1,34 @@
 package view;
 
 import domain.Movie;
-import domain.ReservedInfo;
+import domain.TicketInfo;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 public class OutputView {
-    private static final String ticketInfo = "이름 : %s, 가격 : %s, 시작 시간 : %s, 인원 %s \n";
-    private OutputView(){}
+    private static final String TICKET_INFO_MESSAGE = "이름 : %s, 가격 : %s, 시작 시간 : %s, 인원 %s \n";
+    private static final String PAYMENT_MESSAGE = "결제를 진행합니다. \n";
 
-    public static void printTicketInfo(ReservedInfo reservedInfo){
-        printfMsg(ticketInfo,
-                reservedInfo.getName(),
-                reservedInfo.getPrice(),
-                reservedInfo.getStartTime(),
-                reservedInfo.getNumberOfPeople());
-    }
+    private OutputView(){}
 
     public static void printMovies(List<Movie> movies) {
         for (Movie movie : movies) {
             System.out.println(movie);
         }
+    }
+
+    public static void printTicketInfo(List<TicketInfo> ticketInfos){
+        for(TicketInfo info : ticketInfos){
+            printfMsg(TICKET_INFO_MESSAGE,
+                    info.getName(),
+                    info.getPrice(),
+                    info.getStartTime(),
+                    info.getNumberOfPeople());
+        }
+    }
+
+    public static void AnnouncePayment(){
+        printMsg(PAYMENT_MESSAGE);
     }
 
     public static void printfMsg(String msg, Object... args ) {
